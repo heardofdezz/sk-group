457765566479ada446d0a31d76b682f22fef47c2
heardofdezz <louamax@gmail.com>
2017-12-29 14:49:23 +0100

    continuation back-end set-up du formulaire rstriction and such

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
-class User < ApplicationRecord::Base
-  validates :email, presence: true
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :email, presence: true, email: true, uniqueness: true
+
+  def confirm!
+    update!(confirmed_at: DateTime.now)
+  end
+
+  def confirmed?
+    !! confirmed_at
+  end
 end
